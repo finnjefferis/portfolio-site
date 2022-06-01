478facb6c809d8978606a8cd28831dd0392edc44
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-06-01 18:17:10 +0100

    updated page info

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,39 +6,29 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
-        <script
-          src="https://cdn.jsdelivr.net/npm/github-repo-cards@1.0.9/app.js"
-          defer
-        ></script>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/github-repo-cards@1.0.9/style.css"
-        ></link>
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Hi there,{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            my balls
+            I'm Finn
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+        <p className="mt-3 text-xl">
+          Junior software developer focusing on React and Node.js.
+          
         </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full click:animate-ping">
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold hover:animate-ping">
+            <h3 className="text-2xl font-bold ">
               super-quicksort &rarr;
             </h3>
             <p className="mt-4 text-xl">
@@ -50,9 +40,9 @@ const Home: NextPage = () => {
             href="https://nextjs.org/learn"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-2xl font-bold">react-form-builder &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              Forked from Kiho/react-form-builder. Used in version 4 of Admor DentalPlus software
             </p>
           </a>
 
@@ -78,12 +68,7 @@ const Home: NextPage = () => {
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a>
-          <div>
-            <div>v4</div>
-            Version 4 of Admor DentalPlus software. My current project!
-          </div>
-        </a>
+      
       </footer>
     </div>
   )
