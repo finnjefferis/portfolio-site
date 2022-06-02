830b984be0120bf608362ebf6dbd7cee01c409a6
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-06-02 14:38:00 +0100

    updated animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ const Home: NextPage = () => {
           >
              <button
           className={`${
-            anim && "animate-wiggle"
+            anim && "animate-ping"
           } bg-blue-500 p-4 text-white rounded hover:bg-blue-700 hover:shadow-xl`}
           onClick={() => {
             setAnim(true);
