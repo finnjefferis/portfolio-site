c678a784e6022d9c3cdd34ed8fb247ba4429c32d
Finn Jefferis <91462667+finnjefferis@users.noreply.github.com>
2022-10-12 17:38:22 +0100

    updated modal content

--- pages/index.jsx
+++ pages/index.jsx
@@ -222,95 +222,98 @@ const Home = () => {
               learning.
             </div>
             <div className="py-4 text-left">
-              Skills
+              <div className="font-semibold text-center text-xl">Skills </div>
+              
               <div>
                 Experience in full-stack web app development using frameworks
                 such as React and Next.js.
               </div>
-              <div>Designed websites using JavaScript, HTML, and CSS.</div>
-              <div>Comfortable writing clean and efficient SQL.</div>
-              <div>Experience with Electron and writing browser extensions.</div>
+              <div>- Designed websites using JavaScript, HTML, and CSS.</div>
+              <div>- Comfortable writing clean and efficient SQL.</div>
+              <div>- Experience with Electron and writing browser extensions.</div>
               <div>
                 Familiar with other programming languages such as Python, C# and
                 PHP.
               </div>
-              <div>Fast and intentional learner. </div>
-              <div>Focused & Patient.</div>
-              <div>Dedicated team player.</div>
-              <div>Thrives in fast-paced environment.</div>
-              <div>Quick problem solver.</div>
-              <div>High energy.</div>
-              <div>Committed team player.</div>
-              <div>Organised multi-tasker.</div>
+              <div>- Fast and intentional learner. </div>
+              <div>- Focused & Patient.</div>
+              <div>- Dedicated team player.</div>
+              <div>- Thrives in fast-paced environment.</div>
+              <div>- Quick problem solver.</div>
+              <div>- High energy.</div>
+              <div>- Committed team player.</div>
+              <div>- Organised multi-tasker.</div>
               <div>
-                Passionate about software development and always eager to learn
+                - Passionate about software development and always eager to learn
                 something new.
               </div>
             </div>
             <div className=" text-xl font-semibold">
-              Junior Software Developer at Admor DentalPlus 9-5-2022 - Present
+              Junior Software Developer at Admor DentalPlus 
             </div>
+            <div className="text-xl"> 9/5/2022 - 4/10/2022 </div>
             <div className="text-left">
               <div>
-                Assisted in the design and full-stack development of a modern
+                - Assisted in the design and full-stack development of a modern
                 web app for dental practices
               </div>
               <div>
-                Used Next.js and component libraries Syncfusion and Bootstrap
+                - Used Next.js and component libraries Syncfusion and Bootstrap
                 for the front-end.
               </div>
               <div>
-                Wrote SQL statements using PostgreSQL & PGadmin 4 to handle
+               - Wrote SQL statements using PostgreSQL & PGadmin 4 to handle
                 database interaction.
               </div>
-              <div>Translated legacy PHP scripts into modern Javascript.</div>
+              <div>- Translated legacy PHP scripts into modern Javascript.</div>
               <div>
-                Paid careful attention to performance & React best practices to
+                - Paid careful attention to performance & React best practices to
                 ensure an optimised and secure experience.
               </div>
               <div>
-                Wrote a browser extension for deeper system-level integration.
+                - Wrote a browser extension for deeper system-level integration.
               </div>
             </div>
 
             <div className=" text-xl font-semibold">
-              Software Developer Apprentice at The Shore Group 18-8-2021 –
-              5-5-2022
+              Software Developer Apprentice at The Shore Group
             </div>
+            <div className="text-xl"> 18/8/2021 –
+              5/5/2022 </div>
             <div className="text-left">
               <div>
-                Took personal ownership of a project and worked around the clock
+                - Took personal ownership of a project and worked around the clock
                 to ensure it shipped on time and to a high standard.
               </div>
               <div>
-                Developed a bespoke digital signage solution using React and
+                - Developed a bespoke digital signage solution using React and
                 TailwindCSS for the front-end and cutting-edge headless content
                 management systems like Strapi and Keystone to power the
                 back-end.
               </div>
-              <div>Learned on the job continuously throughout the experience.</div>
-              <div>Self-motivated and highly organised</div>
+              <div>- Learned on the job continuously throughout the experience.</div>
+              <div>- Self-motivated and highly organised</div>
               <div>
-                Familiar with modern development tools such as Git and Node.js
+                - Familiar with modern development tools such as Git and Node.js
               </div>
               <div>
-                Cleaned up and maintained our RDBMS to ensure compatibility with
+                - Cleaned up and maintained our RDBMS to ensure compatibility with
                 custom software
               </div>
-              <div>Assisted colleagues with a plethora of general IT issues</div>
+              <div>- Assisted colleagues with a plethora of general IT issues</div>
               <div></div>
             </div>
 
             <div className="text-xl font-semibold">Education</div>
-            <div className="text-left">BCS Level 3 Certificate in Programming </div>
+            <div className="text-left">- BCS Level 3 Certificate in Programming </div>
             <div className="text-left">
-              BCS Level 3 Certificate in Software Development Context and
+              - BCS Level 3 Certificate in Software Development Context and
               Methodologies{' '}
             </div>
-            <div className="text-left">A Levels - 2018 - 2021</div>
-            <div className="text-left">Computer Science</div>
-            <div className="text-left">Electronics </div>
-            <div className="text-left">Engineering CAMTEC</div>
+            <div className="text-left font-semibold">A Levels - 2018 - 2021</div>
+            <div className="text-left">- Computer Science</div>
+            <div className="text-left">- Electronics </div>
+            <div className="text-left">- Engineering CAMTEC</div>
             <div className="text-left"></div>
           </label>
         </label>
@@ -483,7 +486,7 @@ const Home = () => {
                     </div> */}
                   </div>
 
-                  <div className="right-timeline mb-8 flex w-full flex-row-reverse items-center justify-between">
+                  <div className="right-timeline mb-8 flex w-full flex-row-reverse items-center justify-between ml-3">
                     <div className="order-1 w-5/12"></div>
 
                     <div className="order-1 px-1 py-4 md:w-5/12 lg:text-right xs:text-left">
@@ -551,24 +554,24 @@ const Home = () => {
                     </div>
                   </div>
 
-                  <div class="left-timeline mb-8 flex w-full flex-row-reverse items-center justify-between lg:text-right xs:text-left">
-                    <div class="order-1 md:w-5/12"></div>
-                    <div class="order-1 px-1 py-4 md:w-5/12 ">
-                      <div class="mb-3 text-base text-secondary">
+                  <div className="left-timeline mb-8 flex w-full flex-row-reverse items-center justify-between lg:text-right xs:text-left ml-3">
+                    <div className="order-1 md:w-5/12"></div>
+                    <div className="order-1 px-1 py-4 md:w-5/12 ">
+                      <div className="mb-3 text-base text-secondary">
                         {' '}
                         Started September 2018
                       </div>
-                      <h4 class="mb-3   text-lg font-bold md:text-2xl">
+                      <h4 className="mb-3   text-lg font-bold md:text-2xl">
                         Worthing College
                       </h4>
-                      <div class="my-1 text-sm leading-snug  text-opacity-100 md:text-base">
+                      <div className="my-1 text-sm leading-snug  text-opacity-100 md:text-base">
                         - Studied Computer Science, Electronics, and Engineering
                       </div>
-                      <div class="my-1 text-sm leading-snug  text-opacity-100 md:text-base">
+                      <div className="my-1 text-sm leading-snug  text-opacity-100 md:text-base">
                         - Introduced to C# and native app development using
                         WinForms.
                       </div>
-                      <div class="my-1 text-sm leading-snug  text-opacity-100 md:text-base">
+                      <div className="my-1 text-sm leading-snug  text-opacity-100 md:text-base">
                         - Learned principles of SQL and relational databases.
                       </div>
                       <motion.a
@@ -829,7 +832,7 @@ const Home = () => {
       </div>
 
       <div className="section" id="contact-me">
-      <div className="text-center text-5xl">
+      <div className="text-center text-5xl ">
           Get in touch
         </div>
         <div className="  bg-base-100 md:flex"></div>
